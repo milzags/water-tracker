7ea9aa5460b64eaeddc8749773de329720c5686c
milanzagorac <zagoracm@gmail.com>
2020-12-13 19:56:44 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const smallCups = document.querySelectorAll('.cup-small'); //nodelistof elements
+const litres = document.getElementById('litres');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+const goal = document.getElementById('goal');
+
+updateBigCup();
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlightCups(idx))
+    });
+
+function highlightCups(idx) {
+    if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx --;
+    }
+
+    smallCups.forEach((cup, idx2) => {
+        if (idx2 <= idx) {
+            cup.classList.add('full');
+        } else {
+            cup.classList.remove('full');
+        }
+    });
+
+    updateBigCup();
+};
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length;
+    const totalCups = smallCups.length;
+
+    if (fullCups === 0) {
+        percentage.style.visibilty = 'hidden';
+        percentage.style.height = 0;
+    } else {
+        percentage.style.visibility = 'visible';
+        percentage.style.height = `${fullCups / totalCups * 330}px`;
+        percentage.innerText = `${fullCups / totalCups * 100}%`;
+    }
+
+    if (fullCups === totalCups) {
+        remained.style.visibility = 'hidden';
+        remained.style.height = 0;
+        goal.innerText = 'Goal Achieved! Great Job!';
+    } else {
+        remained.style.visibility = 'visible';
+        litres.innerText = `${2 - (250 * fullCups / 1000)}L`;
+        goal.innerText = 'Goal: 2 Litres';
+    }
+};
